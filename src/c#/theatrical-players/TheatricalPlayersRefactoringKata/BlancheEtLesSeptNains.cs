3bb5a472217b11894139968807f611cdedc29973
Laurent Décamps <decamps.laurent@gmail.com>
2025-06-06 14:41:49 +0200

    Sing with me

--- src/c#/theatrical-players/TheatricalPlayersRefactoringKata/BlancheEtLesSeptNains.cs
+++ src/c#/theatrical-players/TheatricalPlayersRefactoringKata/BlancheEtLesSeptNains.cs
@@ -18,10 +18,10 @@ namespace TheatricalPlayersRefactoringKata
                 .Map(z =>
                 {
                     CommonUtilsManagerFactoryHandler._p = ((IReadOnlyDictionary<string, Extension>)Mystuff)[z.PlayId].Type;
-                    var amount = applySoldOut(CommonUtilsManagerFactoryHandler._p, z.Audience)
+                    var thing = applySoldOut(CommonUtilsManagerFactoryHandler._p, z.Audience)
                         .Match(price => price, _ => throw new Exception($"unknown type: {CommonUtilsManagerFactoryHandler._p}"));
 
-                    var credits = NotCrappyTreatment(CommonUtilsManagerFactoryHandler._p, z.Audience);
+                    var app = NotCrappyTreatment(CommonUtilsManagerFactoryHandler._p, z.Audience);
 
                     if (CommonUtilsManagerFactoryHandler._plays == 1)
                     {
@@ -30,16 +30,16 @@ namespace TheatricalPlayersRefactoringKata
                     else if (CommonUtilsManagerFactoryHandler._plays == 3)
                     {
                         return new Ss(
-                            l(((IReadOnlyDictionary<string, Extension>)Mystuff)[z.PlayId].Name, amount, z.Audience),
-                            amount,
-                            credits);
+                            l(((IReadOnlyDictionary<string, Extension>)Mystuff)[z.PlayId].Name, thing, z.Audience),
+                            thing,
+                            app);
                     }
                     else
                     {
                         return new Ss(
-                            l(((IReadOnlyDictionary<string, Extension>)Mystuff)[z.PlayId].Name, amount, z.Audience),
-                            amount,
-                            credits);
+                            l(((IReadOnlyDictionary<string, Extension>)Mystuff)[z.PlayId].Name, thing, z.Audience),
+                            thing,
+                            app);
                     }
                 })
                 .Reduce((context, line) => context.Append(line))
